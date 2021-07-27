9bafefe583888ea96aa4498d0acc0687514aef95
kimsh42655 <87419774+kimsh42655@users.noreply.github.com>
2021-07-27 21:52:14 +0900

    second version
    create content pages and add sidebar

--- js/ui.js
+++ js/ui.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    $("#wrap").addClass("start");
+    $("header").addClass("start");
+    $("header #menu_wrap .menu").click(function(){
+        $("header").addClass("clicked");
+        var page = $(this).attr("data-page");
+        $("#content"+page).addClass("clicked");
+        $("aside").addClass("clicked");
+    });
+    $(".content button").click(function(){
+        $("header").removeClass("clicked");
+        $(".content").removeClass("clicked");
+        $("aside").removeClass("clicked");
+    });
+    $("aside .home_button").click(function(){
+        $("header").removeClass("clicked");
+        $(".content").removeClass("clicked");
+        $("aside").removeClass("clicked");
+    });
+    $("header #menu_wrap nav ul li.menu").hover(function(){
+        $("header #menu_wrap nav ul li.menu").css("transition-delay","0s");
+    });
 });
