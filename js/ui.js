8e6fcdbede51f2df327092bf532f5c01916a01ae
kimsh42655 <87419774+kimsh42655@users.noreply.github.com>
2021-07-28 02:05:23 +0900

    third version
    changed sidebar into topbar(?) and made content page title

--- js/ui.js
+++ js/ui.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
     $("header").addClass("start");
-    $("header #menu_wrap .menu").click(function(){
+    $("header .menu_wrap .menu").click(function(){
         $("header").addClass("clicked");
         var page = $(this).attr("data-page");
         $("#content"+page).addClass("clicked");
         $("aside").addClass("clicked");
-    });
-    $(".content button").click(function(){
-        $("header").removeClass("clicked");
-        $(".content").removeClass("clicked");
-        $("aside").removeClass("clicked");
     });
     $("aside .home_button").click(function(){
         $("header").removeClass("clicked");
         $(".content").removeClass("clicked");
         $("aside").removeClass("clicked");
     });
-    $("header #menu_wrap nav ul li.menu").hover(function(){
-        $("header #menu_wrap nav ul li.menu").css("transition-delay","0s");
+    $("header .menu_wrap nav ul li.menu").hover(function(){
+        $("header .menu_wrap nav ul li.menu").css("transition-delay","0s");
+    });
+    $("aside nav ul li.sidemenu a").click(function(){
+        var page = $(this).attr("data-page");
+        $(".content").removeClass("clicked");
+        $("#content"+page).addClass("clicked");
     });
 });
