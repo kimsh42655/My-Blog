7d215f556c00c13fe9bf9ca8da5e12888336d6df
kimsh42655 <87419774+kimsh42655@users.noreply.github.com>
2021-07-29 18:09:54 +0900

    fifth version
    make real content page and connect to another html file to show it

--- js/ui.js
+++ js/ui.js
@@ -10,13 +10,28 @@ $(document).ready(function(){
         $("header").removeClass("clicked");
         $(".content").removeClass("clicked");
         $("aside").removeClass("clicked");
+        $(".content_page").removeClass("clicked");
     });
     $("header .menu_wrap nav ul li.menu").hover(function(){
         $("header .menu_wrap nav ul li.menu").css("transition-delay","0s");
     });
     $("aside nav ul li.sidemenu a").click(function(){
+        $(".content_page").removeClass("clicked");
         var page = $(this).attr("data-page");
         $(".content").removeClass("clicked");
         $("#content"+page).addClass("clicked");
+    });
+    $(".content .content_list .page_link").click(function(){
+        $(".content_page").addClass("clicked");
+        var liurl = $(this).data("url");
+        $(".content_page").html();
+        $.ajax({
+            type: 'get',
+            url: liurl,
+            dataType: 'html',
+            success: function(data){
+                $(".content_page").html(data);
+            }
+        });
     });
 });
